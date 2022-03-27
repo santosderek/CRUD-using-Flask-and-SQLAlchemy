cbca29aeefeb7e7d350892bf8e59121a4e913bb3
Derek Santos <derek@dyrenex.com>
2022-03-27 11:17:11 -0400

    Default to port 80 but .env has 8080.

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HTTP_PORT = getenv('HTTP_PORT', None)
+HTTP_PORT = int(getenv('HTTP_PORT', 80))
 HTTP_DOMAIN = getenv('HTTP_DOMAIN', None)
 HTTP_SCHEME = getenv('HTTP_SCHEME', None)
 FLASK_DEBUG = getenv('FLASK_DEBUG', "False").lower() in ['1', 'true', True]
