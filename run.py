56a26b9d87769e2820e95756fda3a58ab73d46ee
Derek Santos <derek@dyrenex.com>
2022-03-27 11:10:07 -0400

    Forgot debug.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from app import create_application_factory
 from app.settings import HTTP_PORT, HTTP_DOMAIN, HTTP_SCHEME, FLASK_DEBUG
 from app.sql import create_database
@@ -7,7 +8,7 @@ application = create_application_factory()
 
 
 def run(*args, **kwargs):
-    application.run(*args, port=HTTP_PORT, host=HTTP_DOMAIN, **kwargs)
+    application.run(*args, port=HTTP_PORT, host=HTTP_DOMAIN, debug=FLASK_DEBUG, **kwargs)
 
 if __name__ == '__main__':
     run()
